2dce2fb1c40eedf0eb312b9f8d6e0067048068d3
reza pratama <rezap9303@gmail.com>
2024-05-31 16:43:03 +0900

    mengedit file News.jsx

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Container, Row, Col, Card, Form, Button, Spinner } from 'react-bootstrap';
 import axios from 'axios';
 
-const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 class NewsApp extends Component {
   constructor(props) {
