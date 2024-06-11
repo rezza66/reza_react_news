8a657a4c9fdc9c67bbb504fca758cc4e53fcf6fb
reza pratama <rezap9303@gmail.com>
2024-06-11 12:57:59 +0900

    edit file vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY)
-  }
-})
+  plugins: [react()]
+});
