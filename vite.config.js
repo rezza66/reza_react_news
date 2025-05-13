564e0e9653948501ec8973a8217af06870f11c48
reza pratama <rezap9303@gmail.com>
2025-05-13 13:45:40 +0900

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/reza_react_news/',
+  plugins: [react()]
 });
